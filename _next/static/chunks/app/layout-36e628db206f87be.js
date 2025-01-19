beab9e34ff742532908212723b3c9c3f77a9a333
Chiemela Chinedum <chiemela@finscale.ai>
2025-01-19 12:02:13 -0500

    Adds landing page

---
+++ _next/static/chunks/app/layout-36e628db206f87be.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4550:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,347,23)),Promise.resolve().then(l.t.bind(l,5464,23))},347:()=>{},5464:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_179fbf"}}},e=>{var s=s=>e(e.s=s);e.O(0,[314,441,517,358],()=>s(4550)),_N_E=e.O()}]);
